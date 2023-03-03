a43ce69d4ef63a80947baeb922288c06c79fd32d
2riing <2riing2@gmail.com>
2023-03-03 11:00:00 +0900

    feat: discount 기능

--- src/app/discount/discountPolicy/FixedAmountDiscountPolicy.java
+++ src/app/discount/discountPolicy/FixedAmountDiscountPolicy.java
@@ -1,10 +1,10 @@
 package app.discount.discountPolicy;
 
-public class FixedAmountDiscountPolicy {
+public class FixedAmountDiscountPolicy implements DiscountPolicy {
 
     private int discountAmount;
 
-    public FixedAmountDiscountPolicy(int dircountAmount) {
+    public FixedAmountDiscountPolicy(int discountAmount) {
         this.discountAmount = discountAmount;
     }
 
